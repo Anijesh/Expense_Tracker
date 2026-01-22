2db4cfb9ef0952dd199662880d8625a8d6c02842
Anijesh <anijeshkr@gmail.com>
2026-01-23 01:36:50 +0530

    Added authentication

--- core/views.py
+++ core/views.py
@@ -1,7 +1,48 @@
 from django.shortcuts import render, redirect
 from .models import CurrentBalance, TrackingHistory
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
+
+def login_view(request):
+    if request.method =="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user =User.objects.filter(username=username)
+        if not user.exists():
+            messages.error(request,"username not found")
+            return redirect('register_view')
+        user=authenticate(username=username,password=password)
+        if not user:
+            messages.error(request,"incorrect password")
+            return redirect('login_view')
+        login(request,user)
+        return redirect('/')
+    return render(request,'login.html')
+
+def register_view(request):
+    if request.method =="POST":
+        username=request.POST.get('username')
+        user=User.objects.filter(username=username)
+        if user:
+            messages.error(request, "Username already exists")
+            return redirect('login_view')
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        password=request.POST.get('password')
+        user=User.objects.create(username=username,
+                                 first_name=first_name,
+                                 last_name=last_name)
+        user.set_password(password)
+        user.save()
+        messages.success(request, "Registration Successfull")
+        return redirect('login_view')
+    return render(request,'register.html')
+
+
+@login_required(login_url="login_view")
 def index(request):
     # Handle form submission
     if request.method == 'POST':
@@ -67,3 +108,7 @@ def delete_transaction(request, id):
         transaction.delete()
 
     return redirect('/')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
